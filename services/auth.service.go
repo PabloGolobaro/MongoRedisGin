87442bbf33ff960184c995639ff597cb78914142
PabloGolobaro <yeu334@yandex.ru>
2022-11-27 20:22:24 +0300

    Initial commit

---
+++ services/auth.service.go
@@ -0,0 +1,8 @@
+package services
+
+import "MongoRedisGin/models"
+
+type AuthService interface {
+	SignUpUser(*models.SignUpInput) (*models.DBResponse, error)
+	SignInUser(*models.SignInInput) (*models.DBResponse, error)
+}
